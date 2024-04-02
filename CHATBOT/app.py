69be7187ab4b8165b9fc54e0d934825460628460
Subham0003 <subham0003@gmail.com>
2024-04-02 16:50:12 +0530

    updated with other files

---
+++ CHATBOT/app.py
@@ -0,0 +1,8 @@
+from langchain_openai import Chatopenai
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParsers
+
+import streamlit as  st
+import os
+from dotenv import load_dotenv
+
